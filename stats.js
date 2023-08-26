4f30baec3ac2b1d5dd58e30463f428ea13963696
nweber28 <70984558+nweber28@users.noreply.github.com>
2023-08-26 11:43:15 -0400

    Update json stats

--- stats.js
+++ stats.js
@@ -2,7 +2,7 @@
 var playerStats;
 
 $(document).ready(function () {
-  $.getJSON("players.json", function (data) {
+  $.getJSON("bnbStats.json", function (data) {
     // console.log(data);
     // console.log("we in");
     //iterate over each object
