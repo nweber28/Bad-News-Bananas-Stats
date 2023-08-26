ae7afc91ef7e6b5740ccbfd1bdfb3eb08fdc2059
nweber28 <70984558+nweber28@users.noreply.github.com>
2023-08-26 11:29:37 -0400

    Pull stats from json, remove player nums

--- stats.js
+++ stats.js
@@ -6,7 +6,65 @@ $(document).ready(function () {
     // console.log(data);
     // console.log("we in");
     //iterate over each object
+    append_json(data);
   }).fail(function () {
     console.log("fail");
   });
 });
+
+function append_json(data) {
+  var table = document.getElementById("playerStats");
+  data.forEach(function (object) {
+    var tr = document.createElement("tr");
+    tr.innerHTML =
+      "<td>" +
+      object.Player +
+      "</td>" +
+      "<td>" +
+      object.Games +
+      "</td>" +
+      "<td>" +
+      object.PA +
+      "</td>" +
+      "<td>" +
+      object.AB +
+      "</td>" +
+      "<td>" +
+      object.Runs +
+      "</td>" +
+      "<td>" +
+      object.H +
+      "</td>" +
+      "<td>" +
+      object["1B"] +
+      "</td>" +
+      "<td>" +
+      object["2B"] +
+      "</td>" +
+      "<td>" +
+      object["3B"] +
+      "</td>" +
+      "<td>" +
+      object.HR +
+      "</td>" +
+      "<td>" +
+      object.BB +
+      "</td>" +
+      "<td>" +
+      object.SF +
+      "</td>" +
+      "<td>" +
+      object.BA +
+      "</td>" +
+      "<td>" +
+      object.OBP +
+      "</td>" +
+      "<td>" +
+      object.SLG +
+      "</td>" +
+      "<td>" +
+      object.OPS +
+      "</td>";
+    table.appendChild(tr);
+  });
+}
